c12e48129cc679f7a6f0915ca0f5492531ad734b
takryudor <dpmtri12@gmail.com>
2026-03-31 03:45:19 +0700

    fix: merge conflict at mount

--- src/api/okr/index.js
+++ src/api/okr/index.js
@@ -2,12 +2,13 @@ import express from "express";
 import objectiveRoutes from "./objective/objective.route.js";
 import keyResultRoutes from "./key-result/key-result.route.js";
 import checkInRoutes from "./check-in/check-in.route.js";
-import feedbackRoutes from "./feedbacks/feedback.route.js";
+import feedBackRoutes from "./feedbacks/feedback.route.js";
+
 const router = express.Router();
 
 router.use(objectiveRoutes);
 router.use(keyResultRoutes);
 router.use(checkInRoutes);
-router.use(feedbackRoutes);
+router.use(feedBackRoutes);
 
 export default router;
